fe79454e84d8bce8b9657e75fc3864711ecb0423
elCharmandersito <patriciorobertoparrasoto@gmail.com>
2021-07-04 21:05:28 -0400

    Actualización clase certificado
    Agregacion de metodos

--- src/main/java/com/certificados/model/certificados.java
+++ src/main/java/com/certificados/model/certificados.java
@@ -1,5 +1,38 @@
 package com.certificados.model;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
 public class certificados {
+	
+	@Id
+	private int id;
+	
+	@Column(name = "tipoCertificado", length = 100)
+	private String tipoCertificado;
+	
+	@Column(name = "motivo", length = 100)
+	private String motivo;
+	
+	@Column(name = "rutAlumno", length = 10)
+	private String rutAlumno;
+	
+	@Column(name = "rutDocente", length = 10)
+	private String rutDocente;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "rutAlumno")
+	@JsonBackReference
+	private List<estudiante> estudiantes;
+	
 
 }
